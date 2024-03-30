1d5402265e47ad045e9e764ce5563772c76676df
cole <coledecarlo@gmail.com>
2024-03-29 19:55:19 -0700

    possible glitch fix

--- common/util.py
+++ common/util.py
@@ -2,12 +2,13 @@ def generate_ar_code(
     address: int,
     data: bytes
 ) -> bytes:
+    n = len(data)
     data = bytearray(data + b"\0" * (7 - (len(data) - 1) % 8))
     for i in range(0, len(data), 4):
         data[i : i + 4] = data[i + 3 : i - 1 if i else None : -1]
     return b"".join((
         (0xE0000000 | address).to_bytes(4),
-        len(data).to_bytes(4),
+        n.to_bytes(4),
         data,
         b"\xD2\xB4\xC0\x1E\x36\x91\x20\x00"
     ))

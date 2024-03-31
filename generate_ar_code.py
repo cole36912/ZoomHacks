cb0585550397f3b0862591245c9e3c783c7b487e
cole <coledecarlo@gmail.com>
2024-03-31 13:35:55 -0700

    fixes

--- generate_ar_code.py
+++ generate_ar_code.py
@@ -13,8 +13,8 @@ with open("data/camera_data_dppt.bin", "rb") as data:
     with open("ar_codes/pt/control.txt", "w") as file:                        # code for default values, should have no effect
         file.write(util.generate_pretty_ar_code(0x021F8AE4, data))
     arr = CameraArray[CameraDPPt].from_files(io.BytesIO(data))
-    buildings = next(camera for camera in arr.cameras if camera.is_2d)          # get the first (and only) 2d camera
-    arr.cameras = [buildings] * 16                                              # assign to all cameras
+    buildings = next(camera for camera in arr.cameras if camera.is_2d)        # get the first (and only) 2d camera
+    arr.cameras = [buildings] * 16                                            # assign to all cameras
     with open("ar_codes/dp/2d.txt", "w") as file:                             # code to make all cameras the same as the building camera
         file.write(util.generate_pretty_ar_code(0x021F6608, arr.to_bytes()))
     with open("ar_codes/pt/2d.txt", "w") as file:                             # code to make all cameras the same as the building camera
--- generate_data_json.py
+++ generate_data_json.py
@@ -1,7 +1,8 @@
 import json
-from common.camera_codec import CameraArray
+from common.codec.camera_base import CameraArray
+from common.codec.camera_dppt import CameraDPPt
 
-with open("data/camera_names.txt", "r") as names, \
-    open("data/camera_data.bin", "rb") as data, \
-    open("data/camera_data.json", "w") as out:
-    json.dump(CameraArray.from_files(data, names).to_json(), out, indent = 2)
+with open("data/camera_names_dppt.txt", "r") as names, \
+    open("data/camera_data_dppt.bin", "rb") as data, \
+    open("data/camera_data_dppt.json", "w") as out:
+    json.dump(CameraArray[CameraDPPt].from_files(data, names).to_json(), out, indent = 2)

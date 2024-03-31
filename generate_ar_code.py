2ee72a647cd0bebd5de5b89c6d2516a2f81d6bc1
cole <coledecarlo@gmail.com>
2024-03-31 15:08:49 -0700

    using metadata files

--- generate_ar_code.py
+++ generate_ar_code.py
@@ -6,28 +6,35 @@ import common.util as util
 import io
 
 # DPPt
-with open("data/original/binary/camera_data_dppt.bin", "rb") as data:
-    data = data.read(CameraDPPt.byte_length * 16)
+with open("data/original/binary/camera_data_dppt.bin", "rb") as data, \
+    open("data/meta/payload_info_dppt.json", "r") as meta:
+    meta = json.load(meta)
+    addr_dp = int(meta["addresses"]["dp"], 16)
+    addr_pt = int(meta["addresses"]["pt"], 16)
+    data = data.read(CameraDPPt.byte_length * meta["count"])
     with open("ar_codes/dp/control.txt", "w") as file:                        # code for default values, should have no effect
-        file.write(util.generate_pretty_ar_code(0x021F6608, data))
+        file.write(util.generate_pretty_ar_code(addr_dp, data))
     with open("ar_codes/pt/control.txt", "w") as file:                        # code for default values, should have no effect
-        file.write(util.generate_pretty_ar_code(0x021F8AE4, data))
+        file.write(util.generate_pretty_ar_code(addr_pt, data))
     arr = CameraArray[CameraDPPt].from_files(io.BytesIO(data))
     buildings = next(camera for camera in arr.cameras if camera.is_2d)        # get the first (and only) 2d camera
-    arr.cameras = [buildings] * 16                                            # assign to all cameras
+    arr.cameras = [buildings] * meta["count"]                                 # assign to all cameras
     with open("ar_codes/dp/2d.txt", "w") as file:                             # code to make all cameras the same as the building camera
-        file.write(util.generate_pretty_ar_code(0x021F6608, arr.to_bytes()))
+        file.write(util.generate_pretty_ar_code(addr_dp, arr.to_bytes()))
     with open("ar_codes/pt/2d.txt", "w") as file:                             # code to make all cameras the same as the building camera
-        file.write(util.generate_pretty_ar_code(0x021F8AE4, arr.to_bytes()))
+        file.write(util.generate_pretty_ar_code(addr_pt, arr.to_bytes()))
 
 # HGSS
-with open("data/original/binary/camera_data_hgss.bin", "rb") as data:
-    data = data.read(CameraHGSS.byte_length * 17)
+with open("data/original/binary/camera_data_hgss.bin", "rb") as data, \
+    open("data/meta/payload_info_hgss.json", "r") as meta:
+    meta = json.load(meta)
+    addr_hgss = int(meta["addresses"]["hgss"], 16)
+    data = data.read(CameraHGSS.byte_length * meta["count"])
     with open("ar_codes/hgss/control.txt", "w") as file:                        # code for default values, should have no effect
-        file.write(util.generate_pretty_ar_code(0x02206478, data))
+        file.write(util.generate_pretty_ar_code(addr_hgss, data))
     arr = CameraArray[CameraHGSS].from_files(io.BytesIO(data))
     buildings = next(camera for camera in arr.cameras if camera.is_2d)          # get the first 2d camera
-    arr.cameras = [buildings] * 17                                              # assign to all cameras
+    arr.cameras = [buildings] * meta["count"]                                   # assign to all cameras
     with open("ar_codes/hgss/2d.txt", "w") as file:                             # code to make all cameras the same as the building camera
-        file.write(util.generate_pretty_ar_code(0x02206478, arr.to_bytes()))
+        file.write(util.generate_pretty_ar_code(addr_hgss, arr.to_bytes()))
         

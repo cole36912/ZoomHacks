9790041e2b76b4ada4828e3fd43309ed653268d6
cole <coledecarlo@gmail.com>
2024-04-27 00:13:48 -0700

    camera editor

---
+++ app/scripts/data_contexts.py
@@ -0,0 +1,131 @@
+from scripts.html_util import build_element
+
+class DataContext:
+    def __init__(self, value, size: int):
+        self.element = self.create_element(value, size)
+        self.element.onkeyup = self.update_row
+        self.element.onchange = self.update_row
+        self.size = size
+
+    def update_row(self, event):
+        if self.element.validity.valid:
+            value = self.get_value()
+            for context in self.row:
+                if context is not self:
+                    context.set_value(value)
+
+    @staticmethod
+    def create_element(value, size: int):
+        return build_element("input", props = {"value": value})
+
+    def get_value(self):
+        return self.element.value
+
+    def set_value(self, value):
+        self.element.value = value
+
+class IntContext(DataContext):
+    @staticmethod
+    def create_element(value: int, size: int):
+        return build_element("input", props = {
+            "type": "number",
+            "value": value,
+            "max": (1 << 8 * size - 1) - 1,
+            "min": -(1 << 8 * size - 1),
+            "required": True
+        })
+
+    def get_value(self) -> int:
+        return int(self.element.value)
+
+class UIntContext(IntContext):
+    @staticmethod
+    def create_element(value: int, size: int):
+        return build_element("input", props = {
+            "type": "number",
+            "value": value,
+            "max": (1 << 8 * size) - 1,
+            "min": 0,
+            "required": True
+        })
+
+class HexContext(DataContext):
+    def __init__(self, value, size: int):
+        super().__init__(value, size)
+        self.element.onchange = self.captialize
+
+    def captialize(self, event):
+        self.element.value = self.element.value.upper()
+        
+    @staticmethod
+    def create_element(value: int, size: int):
+        return build_element("input", props = {
+            "type": "text",
+            "value": f"{{:0{2 * size}X}}".format(value),
+            "pattern": f"^[0-9a-fA-F]{{{2 * size}}}$"
+        })
+
+    def get_value(self) -> int:
+        return int(self.element.value, 16)
+    
+    def set_value(self, value):
+        self.element.value = f"{{:0{2 * self.size}X}}".format(value)
+
+class FloatContext(DataContext):
+    @staticmethod
+    def create_element(value: int, size: int):
+        return build_element("input", props = {
+            "type": "number",
+            "value": round(value / 4096, 4),
+            "max": (1 << 8 * size - 12) - 2e-4,
+            "min": 0,
+            "step": 1e-4,
+            "required": True
+        })
+
+    def get_value(self) -> int:
+        return round(float(self.element.value) * 4096)
+
+    def set_value(self, value: int):
+        self.element.value = round(value / 4096, 4)
+
+class AngleContext(DataContext):
+    @staticmethod
+    def create_element(value: int, size: int):
+        return build_element("input", props = {
+            "type": "number",
+            "value": round(value * 90 / 0x4000, 3),
+            "max": 360 - 3e-3,
+            "min": 0,
+            "step": 1e-3,
+            "required": True
+        })
+
+    def get_value(self) -> int:
+        return round(float(self.element.value) * 0x4000 / 90)
+
+    def set_value(self, value: int):
+        self.element.value = round(value * 90 / 0x4000, 3)
+
+class BooleanContext(DataContext):
+    @staticmethod
+    def create_element(value: int, size: int):
+        return build_element("input", props = {
+            "type": "checkbox",
+            "checked": bool(value)
+        })
+
+    def get_value(self) -> int:
+        return int(self.element.checked)
+
+    def set_value(self, value: int):
+        self.element.checked = bool(value)
+
+
+DATA_CONTEXTS = {
+    "int": IntContext,
+    "hex": HexContext,
+    "float": FloatContext,
+    "angle": AngleContext,
+    "bool": BooleanContext
+}
--- app/scripts/main.py
+++ app/scripts/main.py
@@ -1,9 +1,10 @@
 from pyscript import document
 import json
 from scripts.common.camera import Camera, CameraArray
-from scripts.html_tools import build_element, make_action_link
+from scripts.html_util import build_element, make_action_link
+from scripts.data_contexts import DATA_CONTEXTS, UIntContext, HexContext
 
-VERSION = "v0.0.3"
+VERSION = "v0.0.4"
 DATA_PATH_META = "data/meta/payload_info_{data}.json"
 DATA_PATH_NAMES = "data/meta/camera_names_{data}.txt"
 DATA_PATH_BINARY_ORIGINAL = "data/original/binary/camera_data_{data}.bin"
@@ -12,95 +13,154 @@ DATA = ("dppt", "hgss")
 MODIFICATIONS = ()
 
 document.getElementById("version").innerHTML = VERSION
-app = document.getElementById("app")
-camera_array = None
 
-def edit_camera(i: int):
-    global camera_array
-    camera = camera_array.cameras[i]
-    app.replaceChildren(
-        build_element("h3", f"Camera {i}"),
-        build_element("label",
-            "Label: ",
-            props = {
-                "for": "label"
-            }
-        ),
-        build_element("input",
-            props = {
-                "id": "label",
-                "type": "text",
-                "value": camera.label
-            }
-        ),
-        build_element("br"),
-        build_element("br"),
-        build_element("table",
-            build_element("tr",
-                build_element("th", "Property"),
-                build_element("th", "Contextual Value"),
-                build_element("th", "Base 10 Value"),
-                build_element("th", "Hexadecimal Value")
-            ),
-            *(
-                build_element("tr",
-                    build_element("td", key),
-                )
-                for key, value in camera.parts.items()
+class App:
+    app = document.getElementById("app")
+    camera_array = None
+
+    @classmethod
+    def size_of(cls, key):
+        value_type = cls.camera_array.payload_info["schema"]["parts"][key]["type"]
+        if value_type in ("int16", "uint16"):
+            return 2
+        if value_type in ("int32", "uint32"):
+            return 4
+
+    @classmethod
+    def context_of(cls, key):
+        part = cls.camera_array.payload_info["schema"]["parts"][key]
+        if "context" in part:
+            return DATA_CONTEXTS[part["context"]]
+        return UIntContext
+    
+    @classmethod
+    def edit_camera(cls, i: int):
+        camera = cls.camera_array.cameras[i]
+        contextual_values = {}
+        for key, value in camera.parts.items():
+            size = cls.size_of(key)
+            row = (
+                cls.context_of(key)(value, size),
+                UIntContext(value, size),
+                HexContext(value, size)
             )
-        )
-    )
-
-def show_cameras():
-    global camera_array
-    app.replaceChildren(
-        build_element("table", *(
-            build_element("tr",
-                build_element("td", i, props = {"className": "id_col"}),
-                build_element("td", camera.label),
-                build_element("td", make_action_link("Edit...", lambda e : edit_camera(i))),
-                build_element("td", make_action_link("Copy..")),
-                build_element("td", make_action_link("Swap..")),
-                build_element("td", make_action_link("Import from file..")),
-                build_element("td", make_action_link("Export to file..")),
-            )
-            for i, camera in enumerate(camera_array.cameras)
-        ))
-    )
-    print(camera_array)
-
-def load_default_data(data):
-    global camera_array
-    with open(DATA_PATH_META.format(data = data), "r") as metadata, \
-        open(DATA_PATH_BINARY_ORIGINAL.format(data = data), "rb") as binary_data:
-        try:
-            with open(DATA_PATH_NAMES.format(data = data), "r") as labels:
-                camera_array = CameraArray.from_files(metadata, binary_data, labels)
-        except FileNotFoundError:
-            print(f"not found, {DATA_PATH_NAMES.format(data = data)}")
-            camera_array = CameraArray.from_files(metadata, binary_data)
-    show_cameras()
-
-def main():
-    app.replaceChildren(
-        data_selector := build_element("select", *(
-            build_element("option",
-                f"{item} default",
+            for context in row:
+                context.row = row
+            contextual_values[key] = row
+        cls.render_nodes(
+            build_element("h3", f"Camera {i}"),
+            build_element("label",
+                "Label: ",
                 props = {
-                    "value": item
+                    "for": "label"
+                }
+            ),
+            build_element("input",
+                props = {
+                    "id": "label",
+                    "type": "text",
+                    "value": camera.label
+                }
+            ),
+            build_element("br"),
+            build_element("br"),
+            build_element("div",
+                build_element("table",
+                    build_element("tr",
+                        build_element("th", "Property"),
+                        build_element("th", "Contextual Value"),
+                        build_element("th", "Base 10 Value"),
+                        build_element("th", "Hexadecimal Value")
+                    ),
+                    *(  
+                        build_element("tr",
+                            build_element("td", key),
+                            *(
+                                build_element("td", contextual_value.element)
+                                for contextual_value in contextual_values[key]
+                            )
+                        )
+                        for key in camera.parts
+                    )
+                ),
+                build_element("br"),
+                build_element("div",
+                    build_element("button",
+                        "Cancel",
+                        props = {
+                            "onclick": lambda e : cls.show_cameras()
+                        }
+                    ),
+                    build_element("button",
+                        "Save"
+                    ),
+                    props = {
+                        "style": "text-align: right;",
+                    }
+                ),
+                props = {
+                    "style": "display: inline-block;"
                 }
             )
-            for item in DATA
-        )),
-        build_element("br"),
-        build_element("button",
-            "Use data",
-            props = {
-                "onclick": lambda e : load_default_data(data_selector.value)
-            }
-        ),
-        build_element("button",
-            "Use data from disk..."
         )
-    )
-main()
+
+    @classmethod
+    def show_cameras(cls):
+        cls.render_nodes(
+            build_element("table", *(
+                build_element("tr",
+                    build_element("td", i, props = {"className": "id_col"}),
+                    build_element("td", camera.label),
+                    build_element("td", make_action_link("Edit...", (lambda x : lambda e : cls.edit_camera(x))(i))),
+                    build_element("td", make_action_link("Copy..")),
+                    build_element("td", make_action_link("Swap..")),
+                    build_element("td", 
+                        make_action_link("Import from file..")
+                    ),
+                    build_element("td", make_action_link("Export to file..")),
+                )
+                for i, camera in enumerate(cls.camera_array.cameras)
+            ))
+        )
+        print(cls.camera_array)
+
+    @classmethod
+    def load_default_data(cls, data):
+        with open(DATA_PATH_META.format(data = data), "r") as metadata, \
+            open(DATA_PATH_BINARY_ORIGINAL.format(data = data), "rb") as binary_data:
+            try:
+                with open(DATA_PATH_NAMES.format(data = data), "r") as labels:
+                    cls.camera_array = CameraArray.from_files(metadata, binary_data, labels)
+            except FileNotFoundError:
+                print(f"not found, {DATA_PATH_NAMES.format(data = data)}")
+                cls.camera_array = CameraArray.from_files(metadata, binary_data)
+        cls.show_cameras()
+
+    @classmethod
+    def render_nodes(cls, *nodes):
+        cls.app.replaceChildren(*nodes)
+
+    @classmethod
+    def main(cls):
+        cls.render_nodes(
+            data_selector := build_element("select", *(
+                build_element("option",
+                    f"{item} default",
+                    props = {
+                        "value": item
+                    }
+                )
+                for item in DATA
+            )),
+            build_element("button",
+                "Use data",
+                props = {
+                    "onclick": lambda e : cls.load_default_data(data_selector.value)
+                }
+            ),
+            build_element("br"),
+            build_element("button",
+                "Use data from disk..."
+            )
+        )
+App.main()
